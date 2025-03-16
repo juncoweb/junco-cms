6a0f5770c18c990cbf8d383cfb34b7cfea58a7ab
mpasciulla <mpasciulla@hotmail.com>
2025-03-16 12:07:22 -0300

    Fixed php version checking in bootstrap.

--- bootstrap.php
+++ bootstrap.php
@@ -20,9 +20,10 @@ $max_php_version = '';
 version_compare(PHP_VERSION, $min_php_version, '>=')
 	or die("Requires PHP {$min_php_version} or above.");
 
-$max_php_version
-	and version_compare(PHP_VERSION, $max_php_version, '<=')
-	or die("Requires PHP {$max_php_version} or lower.");
+if ($max_php_version) {
+	version_compare(PHP_VERSION, $max_php_version, '<=')
+		or die("Requires PHP {$max_php_version} or lower.");
+}
 
 if (is_dir(SYSTEM_ABSPATH . 'app/install')) {
 	$goto = explode('/', $_GET['goto'] ?? '')[0];
